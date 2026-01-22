753efc20a9631c4d850e36d80a52e712cbaac54d
mohammadyousufmahmood <mohammadyousufmahmood@gmail.com>
2026-01-22 21:12:49 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Express mvc clean archtecture design for backend developers to look into it and use it as a template. (Best MVC Pattern)
+
+## Versions:
+this is the beta 0.0.0 version and the example code (API interface, modal, view, controller, logs, repository... ) will be added later.
+you can much your solution with this one for your practise.
---
+++ src/helpers/helper.apiResponse.ts
@@ -0,0 +1,16 @@
+export interface APIResponse {
+  stat_code: number
+  stat_message: string
+  data?: any
+  pagination?: Record<string, any> | null
+}
+
+export const apiResponse = (code: number, message: string, data?: any, pagination?: Record<string, any> | null): APIResponse => 
+  {
+  return {
+    stat_code: code,
+    stat_message: message,
+    data: data || null,
+    pagination: pagination || null
+  }
+}
